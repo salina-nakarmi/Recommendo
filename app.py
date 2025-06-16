1cc52a6d19ffd07ff16cf369064994a0654d20b2
jebishabariya <jebi312@gmail.com>
2025-06-16 12:45:19 +0545

    added the genres from the csv to the frontend

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template , request
-
+from genre_load import load_dataset, get_unique_genre
 
 app = Flask(__name__)
 
-
+df = load_dataset()
+unique_genres = get_unique_genre(df)
 
 @app.route('/')
 def home():
@@ -15,7 +16,7 @@ def home():
 
 @app.route('/choose_genre')
 def genre():
-    return render_template('chooseGenre.html')
+    return render_template('chooseGenre.html', genres=unique_genres)
 
 @app.route("/searchbook", methods= ["GET", "POST"])
 def book():
